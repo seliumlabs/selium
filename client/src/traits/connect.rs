7b3d1ca81d5fdda63e0a173e8a499b754d19cb62
adenh93 <aden.herold1@gmail.com>
2023-07-22 17:16:57 +1000

    feat: added register method to Connect trait interface

--- client/src/traits/connect.rs
+++ client/src/traits/connect.rs
@@ -1,9 +1,12 @@
 use anyhow::Result;
 use async_trait::async_trait;
 
+use crate::aliases::Streams;
+
 #[async_trait]
 pub trait Connect {
     type Output;
 
+    async fn register(self, streams: &mut Streams) -> Result<()>;
     async fn connect(self, host: &str) -> Result<Self::Output>;
 }
