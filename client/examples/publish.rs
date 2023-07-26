3f12bc82d99480daf3a82e20dcd63d180a359876
adenh93 <aden.herold1@gmail.com>
2023-07-26 21:35:56 +1000

    feat: updated examples to use encoder/decoder

--- client/examples/publish.rs
+++ client/examples/publish.rs
@@ -1,6 +1,23 @@
 use anyhow::Result;
 use futures::SinkExt;
+use selium::codecs::BincodeCodec;
 use selium::prelude::*;
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+struct StockEvent {
+    ticker: String,
+    change: f64,
+}
+
+impl StockEvent {
+    pub fn new(ticker: &str, change: f64) -> Self {
+        Self {
+            ticker: ticker.to_owned(),
+            change,
+        }
+    }
+}
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -8,10 +25,12 @@ async fn main() -> Result<()> {
         .map("/acmeco/forge_numbers.wasm")
         .keep_alive(5_000)?
         .with_certificate_authority("certs/ca.crt")?
+        .with_encoder(BincodeCodec::default())
         .connect("127.0.0.1:7001")
         .await?;
 
-    publisher.send("{{\"APL\":\"+53.5\"}}").await?;
+    publisher.send(StockEvent::new("APPL", 3.5)).await?;
+    publisher.send(StockEvent::new("INTC", -9.0)).await?;
     publisher.finish().await?;
 
     Ok(())
--- client/examples/subscribe.rs
+++ client/examples/subscribe.rs
@@ -1,6 +1,14 @@
 use anyhow::Result;
 use futures::StreamExt;
+use selium::codecs::BincodeCodec;
 use selium::prelude::*;
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+struct StockEvent {
+    ticker: String,
+    change: f64,
+}
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -8,11 +16,12 @@ async fn main() -> Result<()> {
         .map("/selium/bonanza.wasm")
         .filter("/selium/dodgy_stuff.wasm")
         .with_certificate_authority("certs/ca.crt")?
+        .with_decoder(BincodeCodec::default())
         .connect("127.0.0.1:7001")
         .await?;
 
-    while let Some(Ok(message)) = subscriber.next().await {
-        println!("{message}");
+    while let Some(Ok(StockEvent { ticker, change })) = subscriber.next().await {
+        println!("NEW STOCK EVENT: \"{ticker} - {change}\"");
     }
 
     subscriber.finish().await?;
