9d6cf87b9fd894fbfa5183a2e8083ee2524bacc7
adenh93 <aden.herold1@gmail.com>
2023-07-26 21:35:09 +1000

    feat: updated Publisher and Subscriber clients to implement encoder and decoder logic

--- client/src/traits/connect.rs
+++ client/src/traits/connect.rs
@@ -1,4 +1,3 @@
-use crate::BiStream;
 use anyhow::Result;
 use async_trait::async_trait;
 
@@ -6,6 +5,5 @@ use async_trait::async_trait;
 pub trait Connect {
     type Output;
 
-    async fn register(self, streams: &mut BiStream) -> Result<()>;
     async fn connect(self, host: &str) -> Result<Self::Output>;
 }
