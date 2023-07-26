75ad2ea3c16a84b3174672281daa3e53fb50d4eb
adenh93 <aden.herold1@gmail.com>
2023-07-26 21:29:17 +1000

    refactor: improved BiStream interface

--- client/src/bistream.rs
+++ client/src/bistream.rs
@@ -9,21 +9,30 @@ use tokio_util::codec::{FramedRead, FramedWrite};
 pub type ReadStream = FramedRead<RecvStream, MessageCodec>;
 pub type WriteStream = FramedWrite<SendStream, MessageCodec>;
 
-pub struct BiStream(pub WriteStream, pub ReadStream);
+pub struct BiStream {
+    write: WriteStream,
+    read: ReadStream,
+}
 
 impl BiStream {
     pub async fn try_from_connection(connection: Connection) -> Result<Self> {
-        let (send, recv) = connection.open_bi().await?;
-        let send_stream = FramedWrite::new(send, MessageCodec);
-        let recv_stream = FramedRead::new(recv, MessageCodec);
-
-        Ok(Self(send_stream, recv_stream))
+        let stream = connection.open_bi().await?;
+        Ok(Self::from(stream))
     }
 
     pub async fn finish(self) -> Result<()> {
-        let write_stream = self.0;
+        let write_stream = self.write;
         write_stream.into_inner().finish().await?;
         Ok(())
+    }
+}
+
+impl From<(SendStream, RecvStream)> for BiStream {
+    fn from((send, recv): (SendStream, RecvStream)) -> Self {
+        let write = FramedWrite::new(send, MessageCodec);
+        let read = FramedRead::new(recv, MessageCodec);
+
+        Self { write, read }
     }
 }
 
@@ -31,19 +40,19 @@ impl Sink<Frame> for BiStream {
     type Error = anyhow::Error;
 
     fn poll_ready(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
-        self.0.poll_ready_unpin(cx)
+        self.write.poll_ready_unpin(cx)
     }
 
     fn start_send(mut self: Pin<&mut Self>, item: Frame) -> Result<(), Self::Error> {
-        self.0.start_send_unpin(item)
+        self.write.start_send_unpin(item)
     }
 
     fn poll_flush(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
-        self.0.poll_flush_unpin(cx)
+        self.write.poll_flush_unpin(cx)
     }
 
     fn poll_close(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
-        self.0.poll_close_unpin(cx)
+        self.write.poll_close_unpin(cx)
     }
 }
 
@@ -51,10 +60,10 @@ impl Stream for BiStream {
     type Item = Result<Frame>;
 
     fn poll_next(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
-        self.1.poll_next_unpin(cx)
+        self.read.poll_next_unpin(cx)
     }
 
     fn size_hint(&self) -> (usize, Option<usize>) {
-        self.1.size_hint()
+        self.read.size_hint()
     }
 }
