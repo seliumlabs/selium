8a0fe81fda84aad8e1739bcd080407b7f58b2a17
adenh93 <aden.herold1@gmail.com>
2023-07-26 21:31:59 +1000

    feat: added MessageEncoder and MessageDecoder traits

--- client/src/traits/mod.rs
+++ client/src/traits/mod.rs
@@ -2,8 +2,10 @@ mod client;
 mod client_config;
 mod connect;
 mod into_timestamp;
+mod codec;
 
 pub use client::*;
 pub use client_config::*;
 pub use connect::*;
 pub use into_timestamp::*;
+pub use codec::*;
