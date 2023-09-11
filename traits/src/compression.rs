347a6b7ae735a36abe1304290328eb685412fe53
adenh93 <aden.herold1@gmail.com>
2023-09-12 09:43:07 +1000

    feat: added CompressionLevel trait to compression traits

--- traits/src/compression.rs
+++ traits/src/compression.rs
@@ -8,3 +8,13 @@ pub trait Compress {
 pub trait Decompress {
     fn decompress(&self, input: Bytes) -> Result<Bytes>;
 }
+
+pub trait CompressionLevel {
+    type Target;
+
+    fn highest_ratio(self) -> Self::Target;
+    fn balanced(self) -> Self::Target;
+    fn fastest(self) -> Self::Target;
+    fn level(self, level: u32) -> Self::Target;
+}
+
