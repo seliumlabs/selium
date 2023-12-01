d7a1d86841d3017b8b6eb20e716a7fac85bf70d4
adenh93 <aden.herold1@gmail.com>
2023-12-01 13:52:30 +1100

    fix: moved cloud certs to top level of client directory to avoid gitignore pattern

--- client/src/constants.rs
+++ client/src/constants.rs
@@ -2,11 +2,11 @@
 pub const KEEP_ALIVE_DEFAULT: u64 = 5_000;
 
 #[cfg(debug_assertions)]
-pub(crate) const CLOUD_CA: &[u8; 455] = include_bytes!("certs/cloud.debug.der");
+pub(crate) const CLOUD_CA: &[u8; 455] = include_bytes!("../cloud.debug.der");
 #[cfg(debug_assertions)]
 pub(crate) const SELIUM_CLOUD_REMOTE_URL: &str = "127.0.0.1:4001";
 
 #[cfg(not(debug_assertions))]
-pub(crate) const CLOUD_CA: &[u8; 455] = include_bytes!("certs/cloud.prod.der");
+pub(crate) const CLOUD_CA: &[u8; 455] = include_bytes!("../cloud.prod.der");
 #[cfg(not(debug_assertions))]
 pub(crate) const SELIUM_CLOUD_REMOTE_URL: &str = "selium.com";
