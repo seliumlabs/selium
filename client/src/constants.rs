4852d12829e870f7097656944435f6bb22c903e9
adenh93 <aden.herold1@gmail.com>
2023-12-01 16:39:02 +1100

    chore: updated cloud remote url in debug builds

--- client/src/constants.rs
+++ client/src/constants.rs
@@ -4,7 +4,7 @@ pub const KEEP_ALIVE_DEFAULT: u64 = 5_000;
 #[cfg(debug_assertions)]
 pub(crate) const CLOUD_CA: &[u8; 455] = include_bytes!("../cloud.debug.der");
 #[cfg(debug_assertions)]
-pub(crate) const SELIUM_CLOUD_REMOTE_URL: &str = "127.0.0.1:4001";
+pub(crate) const SELIUM_CLOUD_REMOTE_URL: &str = "127.0.0.1:7002";
 
 #[cfg(not(debug_assertions))]
 pub(crate) const CLOUD_CA: &[u8; 455] = include_bytes!("../cloud.prod.der");
