4c4ca6989fbab588266a86953687ab221b152929
Pete Hayes <pete@hayes.id.au>
2023-12-16 20:40:15 +1100

    New proxy certs

--- client/src/constants.rs
+++ client/src/constants.rs
@@ -6,11 +6,11 @@ pub const KEEP_ALIVE_DEFAULT: u64 = 5_000;
 pub const RETENTION_POLICY_DEFAULT: u64 = 0;
 
 #[cfg(debug_assertions)]
-pub(crate) const CLOUD_CA: &[u8; 455] = include_bytes!("../cloud.debug.der");
+pub(crate) const CLOUD_CA: &[u8; 469] = include_bytes!("../../proxy.debug.der");
 #[cfg(debug_assertions)]
 pub(crate) const SELIUM_CLOUD_REMOTE_URL: &str = "127.0.0.1:7002";
 
 #[cfg(not(debug_assertions))]
-pub(crate) const CLOUD_CA: &[u8; 455] = include_bytes!("../cloud.prod.der");
+pub(crate) const CLOUD_CA: &[u8; 469] = include_bytes!("../../proxy.prod.der");
 #[cfg(not(debug_assertions))]
 pub(crate) const SELIUM_CLOUD_REMOTE_URL: &str = "selium.io:7001";
