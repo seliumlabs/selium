c3eea842aaaf05c3d105fffa7a75284767c530ad
adenh93 <aden.herold1@gmail.com>
2024-03-05 14:41:00 +1100

    feat: added crc to messages for use in validation at a later stage

--- log/src/message/mod.rs
+++ log/src/message/mod.rs
@@ -2,36 +2,52 @@ mod codec;
 mod headers;
 mod slice;
 
-use bytes::{Buf, Bytes, BytesMut};
+use bytes::{Buf, BufMut, Bytes, BytesMut};
 pub use codec::LogCodec;
+use crc32c::crc32c;
 pub use headers::Headers;
 pub use slice::MessageSlice;
 
 pub const LEN_MARKER_SIZE: usize = 8;
+pub const CRC_SIZE: usize = 4;
 pub const HEADERS_SIZE: usize = 24;
 
 #[derive(Debug, Clone)]
 pub struct Message {
     headers: Headers,
     records: Bytes,
+    _crc: u32,
 }
 
 impl Message {
     pub fn new(headers: Headers, records: &[u8]) -> Self {
         let records = Bytes::copy_from_slice(records);
-        Self { headers, records }
+
+        Self {
+            headers,
+            records,
+            _crc: 0,
+        }
     }
 
     pub fn decode(src: &mut BytesMut, length: u64) -> Self {
         let headers = Headers::decode(src, length);
-        let records_byte_len = length as usize - HEADERS_SIZE;
+        let records_byte_len = length as usize - HEADERS_SIZE - CRC_SIZE;
         let records = src.copy_to_bytes(records_byte_len);
-        Self { headers, records }
+        let _crc = src.get_u32();
+
+        Self {
+            headers,
+            records,
+            _crc,
+        }
     }
 
     pub fn encode(&self, buffer: &mut BytesMut) {
         self.headers.encode(buffer);
         buffer.extend_from_slice(&self.records);
+        let crc = crc32c(&buffer);
+        buffer.put_u32(crc);
     }
 
     pub fn records(&self) -> &[u8] {
